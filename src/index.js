6095a7787f0a72771206f441fa85eb82cea08490
Rommanus Anthony <thonyromes@gmail.com>
2020-08-10 19:39:22 +0100

    used less instead of scss for heroku

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.css";
-// import "./assets/css/main.less";
+import "./assets/css/main.scss";
 
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
